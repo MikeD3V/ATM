e927f9b1b419b1e71ae474e7fd5bcc89c947a98a
Mike <emanmike24@gmail.com>
2025-03-12 09:29:47 +0800

    new version

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
+
+
+
 var money = 500;
+var userMoney = document.getElementById('money');
+
+userMoney.innerText = '$' + money;
 
 
-
-function chechBalance() {
-    alert(money);
-}
 
 function withraw() {
-    var withrawnMoney = window.prompt("How much: ");
 
+    var withrawnMoney = window.prompt("How much: ");
+    
 
     if (money >= withrawnMoney) {
         money -= withrawnMoney;
-        alert("New Money: " + money)
+        userMoney.innerText = '$' + money;
     }
     else{
         alert("Not enough money");
     }
-
 }
 
 function deposit() {
     var depositMoney = parseFloat(window.prompt("How much: "));
 
-
-
-    if(depositMoney <= 0){
+    if(isNaN(depositMoney) || depositMoney <= 0){
         alert("Invalid amount");
     }
     else{
         money += depositMoney;
-        alert("New Money: " + money)
+        userMoney.innerText = '$' + money;
     }
 }
 
